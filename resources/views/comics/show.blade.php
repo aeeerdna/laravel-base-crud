a7fd53e332ec8256e14a99af8621679ae61e4079
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-13 21:33:43 +0100

    comic show dati tabella stampati in pagina

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> {{ $comic->title }} </title>
+</head>
+<body>
+    <main>
+        {{-- immagine fumetto --}}
+        <div>
+            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} cover">
+        </div>
+        {{-- titolo --}}
+        <h2>
+            {{ $comic->title }}
+        </h2>
+
+        {{-- descrizione --}}
+        <div>
+            <p>
+                {{ $comic->description }}
+            </p>
+        </div>
+
+        {{-- prezzo --}}
+        <div>
+            <p>
+                {{ $comic->price }} &euro;
+            </p>
+        </div>
+
+    </main>
+</body>
+</html>
