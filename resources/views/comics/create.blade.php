b0d04081224d5e4c4b18a11aa31b11052a338383
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-14 03:22:37 +0100

    parte 2 (es 08-11)
    creazione input per generare nuovo comic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,9 +12,44 @@
             {{-- validazione richiesta utente --}}
             @csrf
 
-            {{-- input del form --}}
+            {{-- input title --}}
             <div>
-                <input type="submit" value="">
+                <input type="text" name="title" placeholder="Title here...">
+            </div>
+
+            {{-- input description --}}
+            <div>
+                <textarea name="description" cols="25" rows="10" placeholder="Description here..."></textarea>
+            </div>
+
+            {{-- input thumb --}}
+            <div>
+                <input type="url" name="thumb" placeholder="Thumb link here..." pattern="https://.*">
+            </div>
+
+            {{-- input price --}}
+            <div>
+                <input type="number" name="price" placeholder="Price here...">
+            </div>
+
+            {{-- input series --}}
+            <div>
+                <input type="text" name="series" placeholder="Series here...">
+            </div>
+
+            {{-- input sale_date --}}
+            <div>
+                <input type="date" name="sale_date" placeholder="Sale date here...">
+            </div>
+
+            {{-- input type --}}
+            <div>
+                <input type="text" name="type" placeholder="Type here...">
+            </div>
+
+            {{-- input create --}}
+            <div>
+                <input type="submit" value="Create your new comic!!">
             </div>
         </form>
     </main>
